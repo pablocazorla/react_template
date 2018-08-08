16b351cd289b5734f7921f90d6922ec958312ecc
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2018-08-08 19:34:01 -0300

    Added images folder

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {  Link } from 'react-router-dom'
+import {  Link } from 'react-router-dom';
+import Logo from '../../images/logo.svg';
 
 export default class Header extends Component {
   // constructor(props) {
@@ -12,6 +13,8 @@ export default class Header extends Component {
   // }
   render() {
     return <header>
+      <img src={Logo} alt="Logo" width="50"/>
+      <br/>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
     </header>;
